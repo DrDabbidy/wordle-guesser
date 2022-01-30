727e2792c65514a450408c0b8e744950fe26df5a
Gaetano Rispoli <gaetanorispoli@gaetanos-air.lan>
2022-01-30 15:53:54 -0500

    put entire wordle wordbank into list

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import requests
 r = requests.get("https://www.powerlanguage.co.uk/wordle/main.e65ce0a5.js")
 text = r.text
 
-list_1 = re.search(r"La=\[(.*?)\]", text)
-list_2 = re.search(r"Ta=\[(.*?)\]", text)
+la = re.search(r"La=(\[(.*?)\])", text)
+ta = re.search(r"Ta=(\[(.*?)\])", text)
 
-print(list_1.group())
-print(list_2.group())
+list_la = la.group(1)[2:-2].split("\",\"")
+list_ta = ta.group(1)[2:-2].split("\",\"")
+
+word_list = list_la + list_ta
+
+print(word_list)
